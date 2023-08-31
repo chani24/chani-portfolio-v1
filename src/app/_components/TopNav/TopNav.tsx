733344be6c694648957ee7b0e04ae7fd6a9b1862
unknown <achidera20@gmail.com>
2023-08-31 17:15:25 +0100

    Added videos for featured projects

--- src/app/_components/TopNav/TopNav.tsx
+++ src/app/_components/TopNav/TopNav.tsx
@@ -5,9 +5,9 @@ export default function TopNav() {
   return (
     <div className={styles.top_nav}>
           <div><Link href="/" className="font-medium">CHIDERA ANI</Link></div>
-      <div className={styles.nav_links}><Link href="/projects">Projects</Link>
-        <a href="#about">About</a>
-        <a href="#services">Services</a>
+      <div className={styles.nav_links}>
+        <Link href="#about">About</Link>
+        <Link href="#projects">Projects</Link>
         <a href="mailto:achidera20@gmail.com?subject=Hire Chidera Ani">Contact</a>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,87 @@
 'use client'
 
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from './main.module.css'
-import { motion, Variants } from "framer-motion";
+
+const featuredProjects = [
+  {
+    title: 'KNF Couture',
+    link: '',
+  },
+  {
+    title: 'Neuf Worldwide',
+    link: '',
+  },
+  {
+    title: 'Threshold Schools',
+    link: '',
+  },
+  {
+    title: 'Breeze Africa Web',
+    link: '',
+  },
+  {
+    title: 'De-Fi',
+    link: '',
+  },
+  {
+    title: 'Breeze Africa Dashboard',
+    link: '',
+  }
+]
 
 
 export default function Home() {
   return (
     <main>
-      <motion.div
+      <div id="about"
       className={'relative ' + styles.hero_wrapper}
-      initial={{ y: -100 }}
-  whileInView={{
-    y: 1,
-    transition: {
-      type: "spring",
-      bounce: 0.4,
-      duration: 0.8
-    }
-  }}
-  viewport={{ once: true }}
     >
         <h1 className="text-extra-large">
-          Frontend Developer 
+          <span>F</span>
+          <span>r</span>
+          <span>o</span>
+          <span>n</span>
+          <span>t</span>
+          <span>e</span>
+          <span>n</span>
+          <span>d</span>{' '}
+          <span>D</span>
+          <span>e</span>
+          <span>v</span>
+          <span>e</span>
+          <span>l</span>
+          <span>o</span>
+          <span>p</span>
+          <span>e</span>
+          <span>r</span>
+            
         </h1>
-    </motion.div>
-    <motion.div
+    </div>
+    <div
         className="h-full grid grid-cols-12 relative"
-        initial={{ y: -70 }}
-  whileInView={{
-    y: 1,
-    transition: {
-      type: "spring",
-      bounce: 0.4,
-      duration: 0.5
-    }
-  }}
-  viewport={{ once: true }}
     >
   <div className="col-span-7 text-medium mb-[72px]">I'm all about building modern frontend and fullstack web applications to meet performance, visual, accessibility and security standards. I'm based in Lagos, collaborating with clients from around the world to bring high-quality and timely web projects to fruition.</div>
-     </motion.div>
-      <div><div className={styles.section_title}><h2 className="text-large">Featured projects</h2> <Link href="/projects" className="text-small">View all</Link></div></div>
+     </div>
+      <div id="projects">
+        <div className={styles.section_title}><h2 className="text-large">Featured projects</h2> <Link href="/projects" className="text-small">View all</Link></div>
+        <div className="h-full grid grid-cols-12 gap-x-[70px] gap-y-[40px] mt-[72px]">
+          {featuredProjects.map((details, index) => {
+            return (
+              <div className="col-span-6 w-full" key={index}>
+                 <video className={styles.projects_video} autoPlay loop muted>
+                 <source src="/videos/defi.mp4" />
+                </video>
+                <div>
+                 <a className={"flex items-end "+ styles.projects_link} href={details.link}> <span>{details.title}</span> <span><Image alt="right arrow" height={16}  width={16} src="/images/right-arrow.svg"/></span></a>
+                </div>
+                
+              </div>
+            )
+          })}
+        </div>
+      </div>
        <div className="h-full grid grid-cols-12  mt-[72px]">
   <div className="col-span-5 text-small">Technologies I work with</div>
         <div className="col-span-7 text-medium">
