0dc85bf897c5478e2e58785e120ecd3de10d1b7e
Chidera <achidera20@gmail.com>
2023-10-21 01:27:22 +0100

    fix seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,41 @@
-'use client'
+"use client";
 
-import SmoothScroll from "./_components/SmoothScroll"
-import './globals.css'
-import TopNav from './_components/TopNav/TopNav';
-import Footer from './_components/Footer/Footer';
+import SmoothScroll from "./_components/SmoothScroll";
+import "./globals.css";
+import TopNav from "./_components/TopNav/TopNav";
+import Footer from "./_components/Footer/Footer";
 import LoadingScreen from "./_components/LoadingScreen/LoadingScreen";
 
-import Head from 'next/head'
-
-
+import Head from "next/head";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
-  }) {
-
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <Head>
         <title>Chidera Ani - Frontend developer</title>
-        <meta name='description' content='Chidera Ani is a creative frontend developer skilled in crafting captivating web experiences. With a strong design sensibility and up-to-date tech knowledge, Chidera transforms concepts into user-friendly interfaces. Their expertise results in responsive, accessible websites, backed by collaborative teamwork.'/>
+        <meta
+          property="og:title"
+          content="Chidera Ani - Frontend developer"
+          key="title"
+        />
+        <meta
+          name="description"
+          property="og:description"
+          content="Chidera Ani is a creative frontend developer skilled in crafting captivating web experiences. With a strong design sensibility and up-to-date tech knowledge, Chidera transforms concepts into user-friendly interfaces. Their expertise results in responsive, accessible websites, backed by collaborative teamwork."
+        />
       </Head>
       <body>
         <TopNav />
         <LoadingScreen />
-      <SmoothScroll>
-          
-        {children}
-        <Footer/>
+        <SmoothScroll>
+          {children}
+          <Footer />
         </SmoothScroll>
       </body>
     </html>
-  )
+  );
 }
