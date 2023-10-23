80edde1934d070234961fe48051bbfd62c22d96c
Chidera <achidera20@gmail.com>
2023-10-23 11:11:58 +0100

    metadata edit

--- src/app/PageWrapper.tsx
+++ src/app/PageWrapper.tsx
@@ -6,8 +6,6 @@ import TopNav from "./_components/TopNav/TopNav";
 import Footer from "./_components/Footer/Footer";
 import LoadingScreen from "./_components/LoadingScreen/LoadingScreen";
 
-import Head from "next/head";
-
 export default function RootLayout({
   children,
 }: {
@@ -15,19 +13,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <title>Chidera Ani - Frontend developer</title>
-        <meta
-          property="og:title"
-          content="Chidera Ani - Frontend developer"
-          key="title"
-        />
-        <meta
-          name="description"
-          property="og:description"
-          content="Creative frontend developer skilled in crafting captivating web experiences. With a strong design sensibility and up-to-date tech knowledge, Chidera transforms concepts into user-friendly interfaces. Their expertise results in responsive, accessible websites, backed by collaborative teamwork."
-        />
-      </Head>
       <body>
         <TopNav />
         <LoadingScreen />
